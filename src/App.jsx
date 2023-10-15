991d1d17fbcb09a1447ca5a665e1f0a407c021ea
Amruth Kiran <amruthkiran47@gmail.com>
2023-10-15 23:20:11 +0530

    added footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,30 @@
 import './App.css';
+import Footer from './components/Footer';
 import Navbar from './components/Navbar';
 
 function App() {
   return (
     <>
       <Navbar />
-      <div className="bg-screen-white flex w-full h-screen justify-center align-middle ">
+      <div className="bg-screen-white flex w-full h-[80vh] justify-center align-middle ">
         <div className="hero_content  flex justify-center gap-8 align-middle flex-col mt-[-200px]">
-          <p className='items-center text-center header1 font-poppins'>Discover & Share</p>
-          <span className='items-center text-center header2 font-poppins'>AI Powered Prompts</span>
-          <p className='items-center text-center text-xl'>
+          <p className="items-center text-center header1 font-poppins">
+            Discover & Share
+          </p>
+          <span className="items-center text-center header2 font-poppins">
+            AI Powered Prompts
+          </span>
+          <p className="items-center text-center text-xl">
             PromptMe is a simple, open-source, and privacy-focused prompt
             generator for writers, artists, and more.
           </p>
+
+          <button className="bg-gradient-to-r from-teal-400 to-blue-500 text-white font-semibold py-2 px-4 rounded-md hover:from-teal-500 hover:to-blue-600">
+            Get This Extension From Chrome Store
+          </button>
         </div>
       </div>
+      <Footer />
     </>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="w-full bg-screen-white p-4">
+      <div className="flex w-full justify-center">
+        <h1>Prompt Me</h1>
+        <p className="text-slate-500"> &copy; 2023 All rights reserved</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
